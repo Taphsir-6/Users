8e0b3a0bb38e2a47054f6a3d2af6407fe77c5cb9
Koumba Thiongane <koumbathiongane201@gmail.com>
2025-04-10 21:45:14 +0200

    struration des classes

---
+++ src/main/java/sn/uasz/UtilisateursAPI/UtilisateursApiApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.UtilisateursAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilisateursApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilisateursApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/uasz/UtilisateursAPI/UtilisateursApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.UtilisateursAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilisateursApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
