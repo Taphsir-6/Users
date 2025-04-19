93a93a24603f8f6cd3b2687324974d3d4dc84c17
OUZ4REAL <maneouz185@gmail.com>
2025-04-20 01:41:36 +0200

    Résolution des conflits avec main

--- src/test/java/sn/uasz/utilisateursapi/UtilisateursApiApplicationTests.java
+++ src/test/java/sn/uasz/utilisateursapi/UtilisateursApiApplicationTests.java
@@ -1,4 +1,4 @@
-package sn.uasz.UtilisateursAPI;
+package sn.uasz.utilisateursapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UtilisateursApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Juste pour tester le chargement du contexte Spring Boot
+    }
 }
