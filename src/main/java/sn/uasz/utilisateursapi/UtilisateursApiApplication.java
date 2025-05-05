3c8282b01485840ae7e3fa6793e2566b6ea270f6
Ouz4real <maneouz2185@gmail.com>
2025-05-05 17:50:14 +0200

    Update UtilisateursApiApplication.java
    Last

--- src/main/java/sn/uasz/utilisateursapi/UtilisateursApiApplication.java
+++ src/main/java/sn/uasz/utilisateursapi/UtilisateursApiApplication.java
@@ -1,15 +1,8 @@
 package sn.uasz.utilisateursapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import sn.uasz.utilisateursapi.entities.Enseignant;
-import sn.uasz.utilisateursapi.repositories.EnseignantRepository;
-
-import java.time.LocalDate;
-
 
 @SpringBootApplication
 public class UtilisateursApiApplication {
